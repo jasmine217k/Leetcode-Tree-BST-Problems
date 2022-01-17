9f6bfcc03de4bedb283be05ac21ef90b6d78d1c2
jasmine217k <92430708+jasmine217k@users.noreply.github.com>
2022-01-17 17:52:41 +0530

    Add files via upload

---
+++ 538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void convert(TreeNode *root,int &sum)
+    {
+
+        if(root==NULL)
+            return;
+        convert(root->right,sum);
+
+        sum+=root->val;
+        root->val=sum;
+        convert(root->left,sum);
+
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int sum=0;
+       convert(root,sum);
+        return root;
+    }
+};
